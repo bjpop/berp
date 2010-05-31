24ba972959b781ca3b2d46a6f62b6277b59f4b28
Bernie Pope <florbitous@gmail.com>
2010-05-31 19:50:41 +1000

    Added GHC license to Unique.hs.

--- src/Berp/Base/Unique.hs
+++ src/Berp/Base/Unique.hs
@@ -3,9 +3,10 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module      : Berp.Base
--- Copyright   : (c) 2010 Bernie Pope
--- License     : BSD-style
--- Maintainer  : florbitous@gmail.com
+-- Copyright   : (c) Copyright 2002, The University Court of the University 
+--               of Glasgow.  All rights reserved.  
+-- License     : See the GHC license below. 
+-- Maintainer  : (of this branch) florbitous@gmail.com
 -- Stability   : experimental
 -- Portability : ghc
 --
@@ -13,6 +14,40 @@
 -- Same as the version in GHC but with added show instance.
 --
 -----------------------------------------------------------------------------
+
+{-
+The Glasgow Haskell Compiler License
+
+Copyright 2002, The University Court of the University of Glasgow. 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+- Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+ 
+- Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+ 
+- Neither name of the University nor the names of its contributors may be
+used to endorse or promote products derived from this software without
+specific prior written permission. 
+
+THIS SOFTWARE IS PROVIDED BY THE UNIVERSITY COURT OF THE UNIVERSITY OF
+GLASGOW AND THE CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+UNIVERSITY COURT OF THE UNIVERSITY OF GLASGOW OR THE CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+DAMAGE.
+-}
 
 
 module Berp.Base.Unique (
