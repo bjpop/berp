b52c05be962754b1044ba1b510dc15312e882ca3
Bernie Pope <florbitous@gmail.com>
2011-03-02 18:44:00 +1100

    Added more cases to the type conversion test

--- test/regression/features/arithmetic/type_conversion.py
+++ test/regression/features/arithmetic/type_conversion.py
@@ -1,4 +1,6 @@
-# int `op` float
+# should really have Bool cases in here too
+
+# int `op` float, float `op` int
 
 print(1 - 2.0)
 print(1.0 - 2)
@@ -11,3 +13,31 @@ print(4.0 * 5)
 
 print(6 / 3.0)
 print(6.0 / 3)
+
+# int `op` complex, complex `op` int
+
+print(1 - 2j)
+print(1j - 2)
+
+print(2 + 3j)
+print(2j + 3)
+
+print(4 * 5j)
+print(4j * 5)
+
+print(6 / 3j)
+print(6j / 3)
+
+# float `op` complex, complex `op` float
+
+print(1.0 - 2j)
+print(1j - 2.0)
+
+print(2.0 + 3j)
+print(2j + 3.0)
+
+print(4.0 * 5j)
+print(4j * 5.0)
+
+print(6.0 / 3j)
+print(6j / 3.0)
