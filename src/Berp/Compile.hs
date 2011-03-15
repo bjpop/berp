b0869fc25f20c7beacf185539d06e4cd677b1425
Bernie Pope <florbitous@gmail.com>
2011-03-16 09:19:53 +1100

    Umbrella module for the compiler

---
+++ src/Berp/Compile.hs
@@ -0,0 +1,19 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      : Berp.Compile
+-- Copyright   : (c) 2010 Bernie Pope
+-- License     : BSD-style
+-- Maintainer  : florbitous@gmail.com
+-- Stability   : experimental
+-- Portability : ghc
+--
+-- Umbrella module for the compiler.
+--
+-----------------------------------------------------------------------------
+
+module Berp.Compile
+   ( dummy )
+   where
+
+dummy :: a
+dummy = undefined
